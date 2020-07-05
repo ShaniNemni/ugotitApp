1ec48c818c0eed991a2e76c610d9e59d9c761b05
shani nemni <shanemni1@gmail.com>
2020-07-05 18:35:57 +0300

    init navigation

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+//screens
+import Services from './src/scenes/services/Services';
+import Profile from './src/scenes/profile/Profile';
+
+import SCENCE_KEYS from './src/scenes/scenesManager/SceneConsts';
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={SCENCE_KEYS.PROFILE}>
+        <Stack.Screen name={SCENCE_KEYS.PROFILE} component={Profile} />
+        <Stack.Screen name={SCENCE_KEYS.SERVICES} component={Services} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
