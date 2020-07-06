2d1bb5c78045c1bc452cb29edeae66c9c6828157
shani nemni <shanemni1@gmail.com>
2020-07-06 19:19:37 +0300

    colors

--- src/utils/localStorage/colors/Colors.js
+++ src/utils/localStorage/colors/Colors.js
@@ -2,3 +2,4 @@ export const GRAY_BACKGROUND = '#E5E5E5';
 export const PURPLE_BACKGROUND = '#8B37FF';
 export const WHITE_COLOR = '#FFFFFF';
 export const ERROR_BACKGROUND= '#FFD6D6';
+export const LIGHT_GRAY_BACKGROUND = '#F5F5F5'
