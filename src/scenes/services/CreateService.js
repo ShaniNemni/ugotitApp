65cb2cbfad59e8dd5a23dbaaed6206e995657b5e
shani nemni <shanemni1@gmail.com>
2020-07-08 17:38:47 +0300

    create service store

--- src/scenes/services/CreateService.js
+++ src/scenes/services/CreateService.js
@@ -6,41 +6,57 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import CustomTextInput from '../../component/costumTextInput/CustomTextInput';
 import CustomButton from '../../component/customButton/CustomButton';
 import WeekView from '../../component/weekday/WeekView';
+import { observer } from 'mobx-react';
+import rootStores from '../../stores/Index';
+import { SERVICE_STORE } from '../../stores/Stores';
 
 const placeholder = "service name";
 const buttonText = 'אישור';
-
-const CreateService = ({closeSubview}) => {
-    const [serviceName,setServiceName] = useState("");
+const serviceStore = rootStores[SERVICE_STORE];
+const CreateService = observer (({closeSubview,iosPlatform}) => {
 
     function changeTextInput() {
+        const serviceName = serviceStore.getServiceName;
         return serviceName.length > 0;
     }
 
+    function validServiceForm () {
+        const serviceName = serviceStore.getServiceName;
+        return serviceName.length > 2;
+    }
+
+    function setServiceName(value) {
+        serviceStore.setServiceName(value);
+    }
+
+    const closeIconByPlatform = iosPlatform ? "close-circle" : "closecircle";
+    const closeTypeIconByPlatfrom = iosPlatform ? "Ionicons" : "AntDesign";
+    const iconTypeByPlatform = iosPlatform ? "MaterialCommunityIcons" : "Entypo";
+    const flexdirectionByPlatform = iosPlatform ? {flexDirection:'row'} : {flexDirection:'row-reverse'};
     return(
         <View style={[styles.view]}>
             <TouchableOpacity onPress={() => closeSubview()} style={[styles.iconView]}>
-                <Icon name={"close-circle"} type={"Ionicons"} style={styles.iconStyle}/>
+                <Icon name={closeIconByPlatform} type={closeTypeIconByPlatfrom} style={styles.iconStyle}/>
             </TouchableOpacity>
 
         <View style={{alignItems:'center',height:'80%',width:'100%'}}>
-            <View style={{marginVertical:'5%',flexDirection:'row',height:'20%'}}>
+            <View style={[{marginVertical:'2%',height:'20%'},flexdirectionByPlatform]}>
                 <CustomTextInput changeTextInputStyle={changeTextInput()} onChangeText={text => setServiceName(text)} placeholder={placeholder} width={'60%'}/>
-                <Icon name={"lock"} type={"MaterialCommunityIcons"} style={{marginHorizontal:10}}/>
+                <Icon name={"lock"} type={iconTypeByPlatform} style={{marginHorizontal:10}}/>
             </View>
 
-            <View style={{height:"40%",borderWidth:1}}>
-                {/* <WeekView/> */}
+            <View style={{height:"20%"}}>
+                {<WeekView/>}
             </View>
 
             <View style={styles.buttonPosition}>
-                <CustomButton buttonText={buttonText}/>
+                <CustomButton disabled={!validServiceForm()} buttonText={buttonText}/>
             </View>
         </View>
 
         </View>
     )
-}
+})
 
 export default CreateService;
 
@@ -50,7 +66,7 @@ const styles = StyleSheet.create({
     },
     iconStyle:{
         color:PURPLE_BACKGROUND,
-        fontSize:40
+        fontSize:30
     },
     iconView:{
         width:40,
@@ -60,6 +76,6 @@ const styles = StyleSheet.create({
     },
     buttonPosition:{
         position:'absolute',
-        bottom:0
+        bottom:'5%'
     }
 })
