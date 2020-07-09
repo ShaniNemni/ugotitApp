c3dc06291d2777abefc6d17c0ee450acd6adb3fd
shani nemni <shanemni1@gmail.com>
2020-07-09 19:31:41 +0300

    android icons

--- src/utils/images/Images.js
+++ src/utils/images/Images.js
@@ -2,7 +2,8 @@ const utilsPath = './';
 
 export const allImages = {
     defaultProfile:require(`${utilsPath}profile.png`),
-    loading:require(`${utilsPath}loading_gif.gif`)
+    loading:require(`${utilsPath}loading_gif.gif`),
+    appLogo:require(`${utilsPath}Ugologo.png`)
 };
 
 
