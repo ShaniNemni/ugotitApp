d2d5a61f49d18d40846d2125a797beec2156c55e
shani nemni <shanemni1@gmail.com>
2020-07-11 15:11:43 +0300

    days list

---
+++ src/utils/lists/DayList.js
@@ -0,0 +1,7 @@
+export const DayList = 
+    [{id:1,name:'ראשון',selected:false},
+    {id:2,name:'שני',selected:false},
+    {id:3,name:'שלישי',selected:false},
+    {id:4,name:'רביעי',selected:false},
+    {id:5,name:'חמישי',selected:false},
+    {id:6,name:'שישי',selected:false}];
